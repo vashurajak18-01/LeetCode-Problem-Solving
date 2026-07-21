5570a172cb9776c4fd01deabfe8b0a15cf44da78
Vashu Rajak <vashurajak660@gmail.com>
2026-07-21 22:20:15 +0530

    Time: 18 ms (9.33%) | Memory: 19.2 MB (53.70%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,11 +2,12 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        original = x
+        original = x   # Copy 
         sum = 0
         while x > 0:
             digit = x % 10
 
+            # Constraints Given
             if sum > (2**31 -1 -digit) //10:
                 return False
 
