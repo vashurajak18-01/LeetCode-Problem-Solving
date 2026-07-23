a22f70722fc786b522c95179060c556e1211e356
Vashu Rajak <vashurajak660@gmail.com>
2026-07-23 20:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (60.65%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,25 +3,46 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # ===================================================================================
+        
         # nums.sort()
 
-        count0 = 0
-        count1 = 0
-        count2 = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                count0 += 1
-            elif nums[i] == 1:
-                count1 += 1
-            else:
-                count2 += 1
-        idx = 0
-        for i in range(count0):
-            nums[idx] = 0
-            idx += 1
-        for i in range(count1):
-            nums[idx] = 1
-            idx += 1
-        for i in range(count2):
-            nums[idx] = 2
-            idx += 1
+        #====================================================================================
+        
+        # count0 = 0
+        # count1 = 0
+        # count2 = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         count0 += 1
+        #     elif nums[i] == 1:
+        #         count1 += 1
+        #     else:
+        #         count2 += 1
+        # idx = 0
+        # for i in range(count0):
+        #     nums[idx] = 0
+        #     idx += 1
+        # for i in range(count1):
+        #     nums[idx] = 1
+        #     idx += 1
+        # for i in range(count2):
+        #     nums[idx] = 2
+        #     idx += 1
+
+        # ====================================================================================
+
+        # Using Dutch National Flag Algorithm
+        low = mid = 0
+        high = len(nums)-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low],nums[mid] = nums[mid], nums[low]
+                mid+=1
+                low+=1
+            elif nums[mid]==1:
+                mid += 1
+            elif nums[mid]==2:
+                nums[mid], nums[high]=nums[high], nums[mid]
+                high-=1
+
