4c14563c0f313467b366633fbccba9544b2856f8
Vashu Rajak <vashurajak660@gmail.com>
2026-07-23 09:07:55 +0530

    Time: 6 ms (80.62%) | Memory: 21.1 MB (28.25%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,46 +1,47 @@
 class Solution:
-    from typing import List
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        pos = []
-        neg = []
+        # sorter way to solve 
+        # nums = list(map(lambda x:x*x,nums))
+        # return sorted(nums)
 
-        for num in nums:
-            if num < 0:
-                neg.append(num)
-            
-            else:
-                pos.append(num)
-
-        if len(neg) == 0:
-            return [x*x for x in pos]
-
-        if len(pos) == 0:
-            res = [x*x for x in neg]
-            res.reverse()
-            return res
-        
-        neg = [x*x for x in neg] [::-1]
-        pos = [x*x for x in pos]
-        n = len(neg)
-        m = len(pos)
+        # Simple and easy approach
         result = []
-
-        i= j = 0
-        while i < n and j < m:
-            if neg[i] < pos[j]:
-                result.append(neg[i])
-                i += 1
-            elif neg[i] >= pos[j]:
-                result.append(pos[j])
-                j += 1
-
-        while i < n:
-            result.append(neg[i])
-            i += 1
-        
-        while j < m:
-            result.append(pos[j])
-            j += 1
-        
+        for num in nums:
+            result.append(num*num)
+        result.sort()
         return result
+
+        # Long Method
+        # nums.sort()
+        # neg = []
+        # pos = []
+        # n = len(nums)
+
+        # for num in nums:
+        #     if num < 0:
+        #         neg.append(num)
+        #     else:
+        #         pos.append(num)
+        # if len(neg) == 0:
+        #     return [x*x for x in pos]
+        # if len(pos) == 0:
+        #     return [x*x for x in neg] [::-1]
+
+        # neg = [x*x for x in neg][::-1]
+        # pos = [x*x for x in pos]
+        # i=j=0
+        # result = []
+        # while i < len(neg) and j< len(pos):
+        #     if neg[i] < pos[j]:
+        #         result.append(neg[i])
+        #         i += 1
+        #     else:
+        #         result.append(pos[j])
+        #         j += 1
+        # while i < len(neg):
+        #     result.append(neg[i])
+        #     i+=1
+        # while j < len(pos):
+        #     result.append(pos[j])
+        #     j+=1
+        # return result
