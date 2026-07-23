79efae111a455765a4eb9b275e3fd70531ab72c1
Vashu Rajak <vashurajak660@gmail.com>
2026-07-23 18:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.09%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,23 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0 = 0
-        count1 = 0
-        count2 = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                count0 += 1
-            elif nums[i] == 1:
-                count1 += 1
-            else:
-                count2 += 1
-        idx = 0
-        for i in range(count0):
-            nums[idx] = 0
-            idx += 1
-        for i in range(count1):
-            nums[idx] = 1
-            idx += 1
-        for i in range(count2):
-            nums[idx] = 2
-            idx += 1
+        nums.sort()
+        
+        # count0 = 0
+        # count1 = 0
+        # count2 = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         count0 += 1
+        #     elif nums[i] == 1:
+        #         count1 += 1
+        #     else:
+        #         count2 += 1
+        # idx = 0
+        # for i in range(count0):
+        #     nums[idx] = 0
+        #     idx += 1
+        # for i in range(count1):
+        #     nums[idx] = 1
+        #     idx += 1
+        # for i in range(count2):
+        #     nums[idx] = 2
+        #     idx += 1
