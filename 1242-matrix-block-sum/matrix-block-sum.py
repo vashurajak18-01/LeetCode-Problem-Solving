91af6de11ff14899aedaf23994d21339bb821ea1
Vashu Rajak <vashurajak660@gmail.com>
2026-07-28 20:57:35 +0530

    Time: 7 ms (96.81%) | Memory: 20.5 MB (76.09%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.py
+++ 1242-matrix-block-sum/matrix-block-sum.py
@@ -1,20 +1,26 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
 
-        n = len(mat)
-        m = len(mat[0])
+        m = len(mat)
+        n = len(mat[0])
 
-        ans = [[0] * m for _ in range(n)]
+        pre_sum =[[0] * (n+1) for _ in range(m+1)]
 
-        for i in range(n):
-            for j in range(m):
-                total = 0
+        for i in range(1,m+1):
+            for j in range(1, n+1):
+                pre_sum[i][j] = mat[i-1][j-1] + pre_sum[i-1][j] + pre_sum[i][j-1] - pre_sum[i-1][j-1]
+        
+        ans =[[0] * n  for _ in range(m)]
 
-                for r in range(max(0, i-k), min(n, i+k+1)):
-                    for c in range(max(0, j-k), min(m, j+k+1)):
-                        total += mat[r][c]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                end_i = min(m, i+k)
+                end_j = min(n, j+k)
 
-                ans[i][j] = total
+                start_i = max(1,i-k)
+                start_j = max(1, j-k)
+
+                ans[i-1][j-1] = pre_sum[end_i][end_j]- pre_sum[start_i - 1][end_j]- pre_sum[end_i][start_j - 1]+ pre_sum[start_i - 1][start_j - 1]
 
         return ans
 
