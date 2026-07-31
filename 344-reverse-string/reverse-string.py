4e881704142c70a4d5abae24668e6cf2644d5410
Vashu Rajak <vashurajak660@gmail.com>
2026-07-31 08:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (19.04%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
