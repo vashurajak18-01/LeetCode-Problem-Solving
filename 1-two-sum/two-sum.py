89ea542f005ae84a66e12ff6d38278f914d1a5ef
Vashu Rajak <vashurajak660@gmail.com>
2026-08-24 21:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (41.09%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
         dict = {}  
-
         for i in range(len(nums)):
             
             if target - nums[i] in dict:
@@ -13,3 +11,8 @@ class Solution:
         return [] 
 
        
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
