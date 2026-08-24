af6a14a0c2afa73f6510dac4874409310b3085d7
Vashu Rajak <vashurajak660@gmail.com>
2026-08-24 21:12:06 +0530

    Time: 2 ms (70.06%) | Memory: 19.3 MB (41.55%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -23,4 +23,10 @@ class Solution:
             carry = total //10
             dummy.next = ListNode(balance)
             dummy = dummy.next
+            
         return res.next
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
