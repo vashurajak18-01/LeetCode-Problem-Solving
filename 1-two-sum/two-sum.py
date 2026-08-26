760a94c06f5c1ed1549fd5e38c5123b7c435052f
Vashu Rajak <vashurajak660@gmail.com>
2026-08-26 20:47:20 +0530

    Time: 4 ms (41.97%) | Memory: 20.6 MB (18.03%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -10,9 +10,6 @@ class Solution:
             
         return [] 
 
-       
-
-
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 # Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
