c940974ada602f5a438c8dfecf64b45d40cc61b8
Vashu Rajak <vashurajak660@gmail.com>
2026-08-27 21:00:07 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (76.50%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -15,3 +15,7 @@ class Solution:
             else:
                 return []
         
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
