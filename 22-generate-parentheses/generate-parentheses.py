fa2dae4a9ecf784c4cac58dc593ea2769009129b
Vashu Rajak <vashurajak660@gmail.com>
2026-08-27 19:29:37 +0530

    Time: 4 ms (12.57%) | Memory: 19.4 MB (35.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -9,19 +9,16 @@ class Solution:
                 if total == 0:
                     result.append("".join(brackets))
                 return
-        
             if total > len(brackets)//2:
                 return
             elif total == -1:
                 return
             brackets[idx] = "("
             solve(idx+1, total + 1)
-
             brackets[idx] = ")"
             solve(idx+1, total-1)
 
         solve(0, 0)
-
         return result
 
 # Synced seamlessly with LeetHub Pro
