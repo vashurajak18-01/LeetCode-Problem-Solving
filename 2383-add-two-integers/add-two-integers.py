8d09f4f3d9baf2677b3cc7a062a6f3183a22c80b
Vashu Rajak <vashurajak660@gmail.com>
2026-09-03 21:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (84.89%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
