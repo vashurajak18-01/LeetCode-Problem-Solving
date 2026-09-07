cd4aeaa713ec02978fa4ec357df0a15d79298ff2
Vashu Rajak <vashurajak660@gmail.com>
2026-09-07 19:57:27 +0530

    Time: 229 ms (23.41%) | Memory: 19.8 MB (49.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,18 +1,34 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = set()
-        left = 0
-        max_len = 0
+        # seen = set()
+        # left = 0
+        # max_len = 0
 
-        for right in range(len(s)):
-            while s[right] in seen:
-                seen.remove(s[left])
-                left += 1
+        # for right in range(len(s)):
+        #     while s[right] in seen:
+        #         seen.remove(s[left])
+        #         left += 1
 
-            seen.add(s[right])
-            max_len = max(max_len, right - left + 1)
+        #     seen.add(s[right])
+        #     max_len = max(max_len, right - left + 1)
 
-        return max_len
+        # return max_len
+
+# ==============================================================================
+
+        maxi = 0
+        my_dict = dict()
+        left = right = 0
+
+        while right < len(s):
+            if s[right] in my_dict:
+                left = max(left, my_dict[s[right]]+1)
+
+            maxi = max(maxi, right - left + 1)
+            my_dict[s[right]] = right
+            right += 1
+
+        return maxi
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
