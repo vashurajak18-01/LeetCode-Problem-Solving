2c498103c86926229f1be57a22b6509e5ace1fbe
Vashu Rajak <vashurajak660@gmail.com>
2026-09-08 18:47:41 +0530

    Time: 69 ms (19.13%) | Memory: 22.2 MB (51.06%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -17,7 +17,26 @@ class Solution:
         #     max_length = max(max_length, right - left + 1)
         
         # return max_length
+# ===============================================================================
+        # maxi = 0
+        # left = right = 0
+        # zeroes = 0
 
+        # while right < len(nums):
+        #     if nums[right] == 0:
+        #         zeroes += 1
+        #     while zeroes > k:
+        #         if nums[left] == 0:
+        #             zeroes -= 1
+        #         left += 1
+        #     if zeroes <= k:
+        #         maxi = max(maxi, right - left + 1)
+        #     right += 1
+            
+        # return maxi
+
+
+# ================================================================================
         maxi = 0
         left = right = 0
         zeroes = 0
@@ -25,7 +44,7 @@ class Solution:
         while right < len(nums):
             if nums[right] == 0:
                 zeroes += 1
-            while zeroes > k:
+            if zeroes > k:
                 if nums[left] == 0:
                     zeroes -= 1
                 left += 1
